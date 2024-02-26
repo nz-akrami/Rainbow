6f6d54cf5425e14aca61de471cc0d2a4b680b5b7
TeamA-MEMBER001 <nz.akrami1385@bmw.com>
2024-02-26 21:58:34 +0330

    create entities

---
+++ Rainbow.Domain/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow.Domain.Entities
+{
+    public class Car
+    {
+        public int CarId { get;private set; }
+        public string CarName { get;private set; }
+        public string CarType { get; private set; }
+        public string CarPrice { get;private set; }
+        public string CarDescription { get;private set; }
+        public string CarColor { get;private set; }
+    }
+}
