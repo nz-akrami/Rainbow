3deda7f02c6d8bb55c10d0d4ba92e48d20c636f0
TeamA-MEMBER001 <nz.akrami1385@bmw.com>
2024-03-01 12:25:52 +0330

    create DTO

---
+++ Rainbow.Application/DTO/carDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow.Application.DTO
+{
+    internal class carDTO
+    {
+        public int CarId { get; set; }
+        public string CarName { get;  set; }
+        public string CarType { get;  set; }
+    }
+}
